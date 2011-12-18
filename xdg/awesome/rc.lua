f70a481d69e6d37bfaeb0ba05c5735a3cb787304
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2011-12-18 16:53:09 +0000

    Add screenshot keybinding

--- xdg/awesome/rc.lua
+++ xdg/awesome/rc.lua
@@ -299,6 +299,7 @@ globalkeys = awful.util.table.join(
               end),
 
     awful.key({ }, "Pause",     function () awful.util.spawn_with_shell("xlock -mode blank") end),
+    awful.key({ }, "Print",     function () awful.util.spawn("scrot -e 'mv $f ~'") end),
     awful.key({ modkey, "Shift"   }, "t",     function () awful.util.spawn_with_shell("ws_currentlyInCIT | zenity --text-info") end)
 )
 
