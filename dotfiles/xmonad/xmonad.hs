b5800f73aa611b60865f5150cb605c72f8fb8d32
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-01-18 06:00:24 +0000

    Add window prompt, for switching windows

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -2,21 +2,49 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.MouseResizableTile
+import XMonad.Prompt
+import XMonad.Prompt.Window
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+
+import Data.Char (toLower)
+import Data.List (isInfixOf)
+import Data.List.Split (splitOn)
+
+myModMask :: KeyMask
+myModMask = mod4Mask
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/tom/.xmobarrc"
 
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
+        , terminal   = "emacsclient -c --eval '(eshell \"foo\")'"
         , layoutHook = avoidStruts $ (mouseResizableTile ||| layoutHook defaultConfig)
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
-        , modMask = mod4Mask
+        , modMask = myModMask
         }
+        `additionalKeys`
+        [ ((myModMask .|. shiftMask, xK_g), windowPromptGoto myXPConfig)
+        , ((myModMask .|. shiftMask, xK_b), windowPromptBring myXPConfig)
+        ]
 
-    
+myXPConfig :: XPConfig
+myXPConfig = defaultXPConfig
+    { autoComplete = Just 750000
+    , searchPredicate = orderIndepdentSubstringMatcher
+    }
+
+
+-- orderIndepdentSubstringMatcher "fire trel" "trello - Firefox" -> True
+-- Terrible name. Shut up, naming is hard.
+orderIndepdentSubstringMatcher :: String -> String -> Bool
+orderIndepdentSubstringMatcher inputString windowTitle = all (\word -> word `matchesAtLeastOneWordOf` windowTitle) $ split inputString
+    where
+        word `matchesAtLeastOneWordOf` window = any (isInfixOf word) (split window)
+        split :: String -> [String]
+        split s = splitOn " " $ map toLower s
