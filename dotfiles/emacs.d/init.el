8e2d4b7534898a5583b6c149ce2ebd7f1af5ba51
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-01-18 23:41:10 +0000

    Add emacs.d

---
+++ dotfiles/emacs.d/init.el
@@ -0,0 +1,61 @@
+(global-linum-mode 1)
+
+(ido-mode 1)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+
+(setq inhibit-startup-message t
+      inhibit-startup-echo-message t)
+
+
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(require 'no-easy-keys)
+(no-easy-keys 1)
+
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(defun make-shell (name)
+  "Create a shell buffer named NAME."
+  (interactive "sName: ")
+  (setq name (concat "$" name))
+  (eshell)
+  (rename-buffer name))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'irfc)
+(setq irfc-directory "~/.cache/emacs/rfcs/")
+(setq irfc-assoc-mode t)
+
+(require 'god-mode)
+(global-set-key (kbd "<escape>") 'god-local-mode)
+
+(require 'uniquify)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
