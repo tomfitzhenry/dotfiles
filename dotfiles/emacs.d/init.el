01fbf6391112310306fee6bff38517f176716449
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-01-19 20:45:32 +0000

    C-c C-m   --->   M-x
    https://sites.google.com/site/steveyegge2/effective-emacs

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -61,3 +61,7 @@
 (require 'uniquify)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;; execute-extended-command is M-x
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
