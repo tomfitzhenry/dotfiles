772ef3bafdc21a2144e405ad75af50d0941c87ed
Tom Fitzhenry <git@tom-fitzhenry.me.uk>
2014-12-21 22:02:53 +0000

    Make gnus look not terrible

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -62,4 +62,14 @@
 
 (use-package gnus
   :config
-  (setq gnus-select-method '(nntp "news.gmane.org")))
+  (setq gnus-select-method '(nntp "news.gmane.org")
+        gnus-summary-line-format "%U%R %&user-date;   %-20,20n   %B%-80,80S\n"
+        gnus-user-date-format-alist '((t . "%Y-%m-%d %H:%M"))
+        gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
+        gnus-thread-sort-functions '(gnus-thread-sort-by-date)
+        gnus-sum-thread-tree-false-root ""
+        gnus-sum-thread-tree-indent " "
+        gnus-sum-thread-tree-leaf-with-other "├► "
+        gnus-sum-thread-tree-root ""
+        gnus-sum-thread-tree-single-leaf "╰► "
+        gnus-sum-thread-tree-vertical "│"))
