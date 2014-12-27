57f0ebd0ed00a428327d2f2501c3b62075eaa58e
Tom Fitzhenry <git@tom-fitzhenry.me.uk>
2014-12-27 14:35:10 +0000

    Update melpa URL

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,5 +1,5 @@
 (package-initialize)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (require 'use-package)
