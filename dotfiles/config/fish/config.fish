9803945f38f5e11307966d96172b11cbaa076454
Thomas Fitzhenry <tom@Thomass-MacBook-Pro.local>
2015-11-29 14:36:30 +0000

    Add fzf options to fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -10,6 +10,10 @@ export VISUAL=vim
 export NAME='Tom Fitzhenry'
 export EMAIL='tom@tom-fitzhenry.me.uk'
 
+export FZF_DEFAULT_OPTS="--multi"
+export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 if [ -f ~/.environment.local ]
 	source ~/.environment.local
 end
