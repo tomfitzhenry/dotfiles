6bbb76ea8dedc1652c4b0f6df329c7f1c41284ff
Tom <tom@tom-fitzhenry.me.uk>
2017-07-03 02:32:04 +1000

    Add emacs config

---
+++ dotfiles/emacs.d/init.el
@@ -0,0 +1,18 @@
+(require 'package)
+(setq package-archives '(("gnu", "https://elpa.gnu.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+(package-initialize)
+
+(require 'use-package)
+
+(use-package better-defaults
+  :ensure t)
+
+(use-package gnus
+  :config
+  (setq gnus-select-method '(nntp "news.gmane.org"))
+  (setq gnus-use-cache t))
+
+(use-package evil
+  :config
+  (evil-mode 1))
