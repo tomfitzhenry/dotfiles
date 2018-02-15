819293edc67e15f04a7e01fb7f2c6110d5d25c6b
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2018-02-15 22:09:48 +1100

    Use HTTPS for MELPA

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(setq package-archives '(("gnu", "https://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")))
 (package-initialize)
 
