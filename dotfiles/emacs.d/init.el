ab3cb603faab68927872fd666c3feb79dde0a2e6
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2019-12-02 08:53:16 +1100

    emacs: decrease auto-revert poll frequency

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -10,6 +10,8 @@
 (require 'use-package)
 (recentf-mode 1)
 (global-hl-line-mode t)
+; Reduce frequency of auto-revert interval from 5s to 20 minutes, to save battery. inotify will do most reverts anyway.
+(setq auto-revert-interval (* 60 20))
 (global-auto-revert-mode t)
 (setq make-backup-files nil)
 (setq scroll-step            1
