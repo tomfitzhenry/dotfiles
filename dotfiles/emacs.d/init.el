9c2a16160ded0c168966fe29b530c79e510805d1
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2020-05-01 22:19:41 +1000

    emacs: remove unused org keys

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -94,10 +94,8 @@
   (setq org-agenda-skip-deadline-if-done t)
   (setq org-agenda-show-future-repeats 'next)
   (setq org-agenda-todo-ignore-scheduled 'future)
-  (global-set-key "\C-cl" 'org-store-link)
   (global-set-key "\C-ca" 'org-agenda)
-  (global-set-key "\C-cc" 'org-capture)
-  (global-set-key "\C-cb" 'org-switchb))
+  (global-set-key "\C-cc" 'org-capture))
 
 (use-package project
   :config
