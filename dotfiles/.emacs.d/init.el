249569e46deb48e9a0ada41e5480cc1b2d927c1d
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2021-02-05 09:34:38 +1100

    emacs: use flymake-diagnostic-functions directly

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -33,7 +33,7 @@
 (add-hook 'text-mode-hook 'flymake-mode)
 (add-hook 'text-mode-hook 'flymake-aspell-setup)
 (add-hook 'sh-mode-hook 'flymake-shellcheck-load)
-(add-hook 'emacs-lisp-mode-hook 'package-lint-flymake-setup)
+(add-hook 'flymake-diagnostic-functions 'package-lint-flymake)
 
 ;; imenu
 (global-set-key (kbd "C-c i") 'consult-imenu)
